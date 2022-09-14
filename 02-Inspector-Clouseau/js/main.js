68282508a75e1917c3d3cdf6f85f4f6a5af14286
allison-baker <baker.allison.m@gmail.com>
2022-09-13 18:41:25 -0600

    document properties

--- 02-Inspector-Clouseau/js/main.js
+++ 02-Inspector-Clouseau/js/main.js
@@ -1,7 +1,18 @@
-document.querySelector("#title").innerHTML = "Inspector Clouseau";
+document.querySelector("#title").innerText = "Inspector Clouseau";
 
-let windowSize = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`;
-let offset = `Window offset is ${window.screenX} pixels from the left edge and ${window.screenY} pixels from the top of the display.`;
+function reDisplay() {
+  let windowSize = `Window size is ${window.innerWidth} pixels wide by ${window.innerHeight} pixels tall.`;
+  let offset = `Window offset is ${window.screenX} pixels from the left edge and ${window.screenY} pixels from the top of the display.`;
+  let url = `The URL for this page is ${window.location}.`;
+ 
+  let myWindow = document.querySelector("#myWindow");
+  myWindow.innerText = `${windowSize}\n${offset}\n${url}`;
 
-let myWindow = document.querySelector("#myWindow");
-myWindow.innerHTML = windowSize + '\n' + offset;
+  let docTitle = `Document title is ${document.title}.`;
+  let modified = `This document was last updated on ${document.lastModified}.`;
+
+  let myDoc = document.querySelector('#myDoc');
+  myDoc.innerText = `${docTitle}\n${modified}`;
+}
+
+reDisplay();
