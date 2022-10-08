42bfdb3da9b739f59b0ffa2b86460aa8481e096f
allison-baker <baker.allison.m@gmail.com>
2022-10-07 21:39:27 -0600

    create trees list

--- 05-Arborist/js/main.js
+++ 05-Arborist/js/main.js
@@ -1,2 +1,18 @@
 document.querySelector("#title").innerText = "Arborist";
 document.querySelector("#subtitle").innerText = "Events and Arrays";
+
+// create an array with 4 trees listed, and create references to div elements
+const trees = ['Oak', 'Pine', 'Aspen', 'Bald Cyprus'];
+const errorElement = document.querySelector('#error');
+const resultsElement = document.querySelector('#displayResults');
+
+// display the list of trees inside the displayResults element
+const listTrees = () => {
+    let treeList = '';
+    trees.forEach(tree => {
+        treeList += `${tree}</br>`;
+    })
+    resultsElement.innerHTML = `${treeList}<span>${trees.length} elements long</span>`;
+}
+
+listTrees();
