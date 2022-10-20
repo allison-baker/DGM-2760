da803d1cb37c144b7d2753841cf2a7c815dbf03c
allison-baker <baker.allison.m@gmail.com>
2022-10-19 19:06:28 -0600

    finish nonsense story

--- 06-Nonsense-Story/js/main.js
+++ 06-Nonsense-Story/js/main.js
@@ -1,2 +1,16 @@
 document.querySelector("#title").innerText = "Nonsense Story";
 document.querySelector("#subtitle").innerText = "String Manipulation";
+
+function tellStory() {
+  nounArray = document.querySelector("#nouns").innerHTML.toLowerCase().split(/\s+/);
+  adjArray = document.querySelector("#adjectives").innerHTML.toLowerCase().split(/\s+/);
+  verbArray = document.querySelector("#verbs").innerHTML.toLowerCase().split(/\s+/);
+
+  const myStory = `Once upon a time, there was a <span>${nounArray[0]}</span> named Bertholdt. He was a <span>${adjArray[0]}</span> guy with a
+  <span>${adjArray[1]}</span> <span>${nounArray[1]}</span>. One day, he wanted to <span>${verbArray[0]}</span> to his <span>${nounArray[2]}</span> so he could have some
+  relaxing time to himself. Instead, he saw that a <span>${adjArray[2]}</span> villian used their <span>${nounArray[3]}</span> to steal his
+  <span>${nounArray[4]}</span>! He went home to <span>${verbArray[1]}</span>. But luckily, in his <span>${nounArray[5]}</span>, Bertholdt found a <span>${nounArray[6]}</span>,
+  so the day ended up okay.`;
+
+  document.querySelector("#story").innerHTML = myStory;
+}
