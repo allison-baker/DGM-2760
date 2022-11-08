21250ec6e930e2d320f8689d3bce2149405f5cfb
allison-baker <baker.allison.m@gmail.com>
2022-11-08 10:21:35 -0700

    fix nonsense story

--- 06-Nonsense-Story/js/main.js
+++ 06-Nonsense-Story/js/main.js
@@ -2,9 +2,9 @@ document.querySelector("#title").innerText = "Nonsense Story";
 document.querySelector("#subtitle").innerText = "String Manipulation";
 
 function tellStory() {
-  nounArray = document.querySelector("#nouns").innerHTML.toLowerCase().split(/\s+/);
-  adjArray = document.querySelector("#adjectives").innerHTML.toLowerCase().split(/\s+/);
-  verbArray = document.querySelector("#verbs").innerHTML.toLowerCase().split(/\s+/);
+  nounArray = document.querySelector("#nouns").value.toLowerCase().split(/\s+/);
+  adjArray = document.querySelector("#adjectives").value.toLowerCase().split(/\s+/);
+  verbArray = document.querySelector("#verbs").value.toLowerCase().split(/\s+/);
 
   const myStory = `Once upon a time, there was a <span>${nounArray[0]}</span> named Bertholdt. He was a <span>${adjArray[0]}</span> guy with a
   <span>${adjArray[1]}</span> <span>${nounArray[1]}</span>. One day, he wanted to <span>${verbArray[0]}</span> to his <span>${nounArray[2]}</span> so he could have some
