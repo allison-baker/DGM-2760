1d2587324409bb80589b3598dcfad3552bb3ebd8
allison-baker <baker.allison.m@gmail.com>
2022-11-15 15:09:50 -0700

    finish unit 9

--- 09-Menu-Duplication/js/main.js
+++ 09-Menu-Duplication/js/main.js
@@ -1,1 +1,23 @@
-document.querySelector("#title").innerText = "Truck Power";
+function duplicateMenu() {
+    //get all anchor elements from top menu
+    let topNav = document.querySelectorAll('ul#primaryNavigation li a');
+    
+    //create the new list items for the bottom of the page
+    let bottomNav = document.createElement('ul')
+
+    topNav.forEach(menuItem => {
+        let newItem = document.createElement('li');
+        let newAnchor = document.createElement('a');
+
+        newAnchor.setAttribute('href', menuItem.getAttribute('href'));
+        newAnchor.textContent = menuItem.textContent;
+        newItem.appendChild(newAnchor);
+        console.log(newItem);
+
+        bottomNav.appendChild(newItem);
+        })
+
+    document.querySelector('#smallNavArea').appendChild(bottomNav);
+}
+
+duplicateMenu();
